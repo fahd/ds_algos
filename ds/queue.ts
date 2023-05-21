c6eae97338793900041b8f4c0fe9ed239a8d5666
fahd <asadmansa@icloud.com>
2023-05-21 16:19:39 -0400

    [edit] Edited stack.ts and queue.ts

--- ds/queue.ts
+++ ds/queue.ts
@@ -1,71 +1,79 @@
+// head => B
+// tail => B
+
+// type QNode that   is a generic over T
+// We create our own container, that means we can pass in any value
+// We manage state ourselves
 type QNode<T> = {
   value: T;
   next?: QNode<T>;
-  // enqueue(value: T): void;
-  // deque(): T | undefined;
-  // peek(): T | undefined;
 }
 
-export default class Queue<T> {
+class Queue<T> {
   public length: number;
+  // private head: QNode<T> | undefined;
   private head?: QNode<T>;
   private tail?: QNode<T>;
 
   constructor() {
-    this.head = this.tail = undefined;
     this.length = 0;
+    this.head = this.tail = undefined;
   }
-
+  // O(1)
   enqueue(item: T): void {
-    const node = { value: item } as QNode<T>;
     this.length++;
     
-    if (!this.tail) {
-      this.tail = this.head = node;
-      return;
-    }
-    this.tail.next = node;
-    this.tail = node; 
-  }
+    const node = { value: item } as QNode<T>;
 
-  deque(): T | undefined {
-    if (!this.head) {
-      return undefined;
+    // TypeScript -> have to intentionally check for undefined. It gets confused.
+    if (!this.tail) this.tail = this.head = node;
+      
+    else {
+      this.tail.next = node;
+      this.tail = this.tail.next;
     }
+  }
+  // O(1) 
+  deque(): T | undefined {
+    if (!this.head) return undefined;
 
     this.length--;
-    
+
     const head = this.head;
+    
     this.head = this.head.next;
+    // console.log(
+    //   'head',head
+    // );
+    // console.log(
+    //   'this.head',this.head
+    // );
     head.next = undefined;
+    
+    if (this.length === 0) this.tail = this.head;
+    
+    // can't be head? because we already checked to see if it exists.
     return head.value;
   }
 
   peek(): T | undefined {
+    // if (!this.head) return undefined;
+    // return this.head.value;
     return this.head?.value;
   }
 }
 
 const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
+// A, B are the generics.
+queue.enqueue("A");
+queue.enqueue("B");
+queue.enqueue("C");
+
+
+console.log(queue.deque());
 console.log(
   'queue',queue
 );
-console.log(
-  'queue.deque', queue.deque()
-);
-console.log(
-  'queue.deque', queue.deque()
-);
-console.log(
-  'queue.deque', queue.deque()
-);
-console.log(
-  'queue.peek()', queue.peek()
-);
-
-
-
+// console.log(queue.deque());
+// console.log(queue.deque());
 
--- ds/stack.ts
+++ ds/stack.ts
@@ -1,35 +1,96 @@
-interface Stack {
+type StackNode<T> = {
+  value: T;
+  prev?: StackNode<T>;
+ }
+
+export default class Stack<T> {
+  public length: number;
+  private head?: StackNode<T>;
+  
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+  }
+    
+  // O(1)
+  push(item: T): void {
+    const node = { value:item } as StackNode<T>;
+    if (!this.head) this.head = node;
+    else {
+      node.prev = this.head;
+      this.head = node;
+    }
+    this.length++;
+  }
+
+  // O(1) 
+  pop(): T | undefined {
+    if (!this.head) return undefined;
+    this.length--;
+
+    const node = this.head;
+    this.head = this.head.prev;
+
+    // free up memory
+    node.prev = undefined;
+    // return value
+    return node.value;
+    
+  }
+
+  peek(): T | undefined {
+    // if (!this.head) return undefined;
+    // return this.head.value;
+    return this.head?.value;  
+  }
+
+  size(): number {
+    return this.length;
+  }
+}
+
+/*
+  interface Stack {
+  // storage: {[number]: boolean};
   storage: {
     [key: number]: number
   };
   stack_size: number;
 }
 
-class Stack {
-  constructor() {
-    this.storage = {};
-    this.stack_size = 0;
-  }
-  push(value: number): void {
-    this.storage[this.stack_size++] = value;
-  }
-  pop(): number | void {
-    if (this.stack_size) {
-      const val = this.storage[this.stack_size - 1];
-      delete this.storage[this.stack_size - 1];
-      this.stack_size--;
-      return val;
+  class Stack {
+    constructor() {
+      this.storage = {};
+      this.stack_size = 0;
+    }
+    push(value: number): void {
+      this.storage[this.stack_size++] = value;
+    }
+    pop(): number | void {
+      if (this.stack_size) {
+        const val = this.storage[this.stack_size - 1];
+        delete this.storage[this.stack_size - 1];
+        this.stack_size--;
+        return val;
+      }
+    }
+    size(): number {
+      return this.stack_size;
     }
   }
-  size(): number {
-    return this.stack_size;
-  }
-}
 
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-console.log(
-  'stack',stack
-);
+  const stack = new Stack();
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  console.log(
+    'stack',stack
+  );
+
+  stack.pop();
+  stack.pop();
+  stack.pop();
+  console.log(
+    'stack after pop',stack
+  );
+*/
