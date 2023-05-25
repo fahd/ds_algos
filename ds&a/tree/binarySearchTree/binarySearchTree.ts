0ce30558f138f8fd8e0b78321413c5d026547b8f
fahd <asadmansa@icloud.com>
2023-05-25 18:43:37 -0400

    [add] Added binarySearchTree for searching

---
+++ ds&a/tree/binarySearchTree/binarySearchTree.ts
@@ -0,0 +1,25 @@
+// Append -> O(h) (down one branch), worst case O(log n) (balanced)
+
+type BinaryNode<T> = {
+  value: T;
+  left: BinaryNode<T> | null;
+  right: BinaryNode<T> | null;
+}
+
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+  if (!curr) return false;
+  if (curr.value === needle) return true;
+
+  if (needle > curr.value) {
+    return search(curr.right, needle);
+  }
+  if (needle < curr.value) {
+    return search(curr.left, needle);
+  }
+
+  return false;
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
+}
