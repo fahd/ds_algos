8e09575882b3fb4e64ce0bc8fd854e766f99dc2a
fahd <asadmansa@icloud.com>
2023-06-07 00:30:20 -0400

    [edit] edited evalRPN

--- lc_problems/150 - evalRPN.ts
+++ lc_problems/150 - evalRPN.ts
@@ -18,45 +18,20 @@ Loop through items in array
     take this value and push back onto stack
 */
 
-function performOperation(operation: string, first: number, second: number): number | undefined {
-  switch (operation) {
-    case "*":
-      return first * second;
-    case "/":
-      return first / second;
-    case "+":
-      return first + second;
-    case "-":
-      return first - second;
-  }
-  return 0;
-}
-
-function roundNumber(n: number): number {
-  if (Math.abs(n) < 1) return 0;
-  if (n < 0) return Math.ceil(n);
-  else return Math.floor(n);
-}
-
-
 function evalRPN(tokens: string[]): number {
-  const stack:number[] = [];
-  
-  for (let i = 0; i < tokens.length; i++){
-    const char = tokens[i];
-    
-    if ((parseInt(char) % 1) === 0) {
-      stack.push(parseInt(char));
-      continue;
-    }
-    // if we have an operator
-    const second = stack.pop() as number;
-    const first = stack.pop() as number;
-    const value = performOperation(char, first, second) as number;
-    let next = roundNumber(value);
-    stack.push(next);
-  }
+  const stack: number[] = [];
 
+  for (const token of tokens) {
+    const num = parseInt(token, 10);
+    if (!isNaN(num)) stack.push(num);
+    else {
+      const second = stack.pop() as number;
+      const first = stack.pop() as number;
+      if (token === '*') stack.push(first * second);
+      if (token === '+') stack.push(first + second);
+      if (token === '-') stack.push(first - second);
+      if (token === '/') stack.push(Math.trunc(first/second));
+    }
+  }
   return stack.pop() as number;
 };
-
