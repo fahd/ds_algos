20fd72bbbb7b5312ba42d81b1ac7f1b1f147e8d1
fahd <asadmansa@icloud.com>
2023-06-08 15:17:32 -0400

    [edit] edited #200 for mutating original array

--- lc_problems/200 - numIslands/dfs.ts
+++ lc_problems/200 - numIslands/dfs.ts
@@ -3,76 +3,51 @@
 // Time Complexity -> O(m x n)
 // Space Complexity -> O(m x n)
 
-// in this example, we didn't mutate the original matrix. 
-  // we can avoid using seen array by mutating the original array if allowed.
-
 type Point = {
-  x: number,
-  y: number
+  x: number;
+  y: number;
 }
 
 const dirs = [
-  [0, 1],  // down
-  [0, -1], // up
-  [1, 0], // right
-  [-1, 0] // left
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0]
 ];
 
-function dfs(
-  grid: string[][],
-  curr: Point,
-  end: Point,
-  seen: boolean[][],
-): void {
-  // if out of bounds x
-  if (curr.x < 0 || curr.x >= end.x) return;
-
-  // if out of bounds y
-  if (curr.y < 0 || curr.y >= end.y) return;
-
-  // if already seen
-  if (seen[curr.y][curr.x]) return;
+const dfs = (grid:string[][], curr: Point, end: Point): void => {
+  const { x, y } = curr;
+  // if out of bounds, return
+  if ((x < 0 || x >= end.x) || (y < 0 || y >= end.y )) {
+    return;
+  }  
+  // if position is already 0, return
+  if (grid[y][x] === '0') {
+    return;
+  }
   
-  // mark item as seen to prevent revisiting in future
-  seen[curr.y][curr.x] = true;
+  // if position is 1, set it to 0
+  grid[y][x] = '0';
   
-  // if we are at a 0, prevent recursing throughout graph
-  // we only want to recurse starting from a position on the island
-  if (grid[curr.y][curr.x] === '0') return;
-
-  // if on island, recurse through graph, visit surrounding nodes up to and including adjacent non-island positions
-  for (let i = 0; i < dirs.length; i++){
-    let [x, y] = dirs[i];
-    dfs(grid, { x: curr.x + x, y: curr.y + y }, end, seen);
+  // go through directions
+  for (const dir of dirs) {
+    let [dX, dY] = dir;
+    dfs(grid, { x: x + dX, y: y + dY }, end);
   }
 }
 
-export default function numIslands(grid: string[][]): number {
-  const seen: boolean[][] = new Array(grid.length).fill(false);
+const numIslands = function (grid: string[][]): number {
   let count = 0;
-  for (let i = 0; i < seen.length; i++){
-    seen[i] = new Array(grid[0].length).fill(false);
-  }
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[0].length; x++){
-      // root node triggers a dfs
-      if (!seen[y][x] && grid[y][x] === '1') {
+  const endRow = grid.length;
+  const endCol = grid[0].length;
+
+  for (let y = 0; y < endRow; y++){
+    for (let x = 0; x < endCol; x++){
+      if (grid[y][x] === '1') {
         count++;
-        dfs(grid, { x, y }, { x: grid[0].length, y: grid.length }, seen)
+        dfs(grid, { x, y }, { x: endCol, y: endRow });
       }
     }
   }
-  
   return count;
 };
-
-const grid = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]
-
-console.log(
-  'numIslands', numIslands(grid)
-);
