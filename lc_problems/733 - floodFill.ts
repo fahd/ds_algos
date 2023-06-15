374d3f653a913302d27d9b64661d9a6f1d2d1567
fahd <asadmansa@icloud.com>
2023-06-15 23:46:00 +0100

    [edit] edited floodFill to dfs

--- lc_problems/733 - floodFill.ts
+++ lc_problems/733 - floodFill.ts
@@ -10,32 +10,22 @@ type Point = {
   y: number;
 }
 
-function bfs(image: number[][], start: Point, end: Point, color: number, startValue: number):void {
-  let q = [start];
-  image[start.y][start.x] = color;
+function dfs(image: number[][], start: Point, end: Point, color: number, startValue: number):void {
+  if (start.x < 0 || start.x >= end.x) return;
+  if (start.y < 0 || start.y >= end.y) return;
+  if (image[start.y][start.x] === color) return;
+  if (image[start.y][start.x] != startValue) return;
   
-  while (q.length){
-    let { x, y } = q.shift() as Point;
+  image[start.y][start.x] = color;
 
-    for (const dir of dirs) {
-      let [dX, dy] = dir;
-
-      let nX = x + dX;
-      let nY = y + dy;
-
-      if (nX < 0 || nX >= end.x) continue;
-      if (nY < 0 || nY >= end.y) continue;
-      if (image[nY][nX] === color) continue;
-      if (image[nY][nX] != startValue) continue;
-
-      image[nY][nX] = color;
-      q.push({ x: nX, y: nY });
-    }
+  for (const dir of dirs) {
+    let [dX, dY] = dir;
+    dfs(image, { x: start.x + dX, y: start.y + dY }, end, color, startValue);
   }
 }
 
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
   if (image[sr][sc] === color) return image;
-  bfs(image, { x: sc, y: sr }, { x: image[0].length, y: image.length }, color, image[sr][sc]);
+  dfs(image, { x: sc, y: sr }, { x: image[0].length, y: image.length }, color, image[sr][sc]);
   return image;
 };
