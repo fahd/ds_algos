aaa1e78eafff0c28204e85206e85ee1a8eb7d2b3
Asad <asadhabibs@Asads-MacBook-Air.local>
2023-06-16 10:56:08 -0400

    [edit] edited twoSum.ts to include time complexity

---
+++ lc_problems/1. twoSum.ts
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/two-sum/
+
+// Time O(n)
+// Space O(n)
+
+const twoSum = (array: number[], target: number): number[] | void => {
+  const map:{[key:number]: number} = {};
+  for (let i = 0; i < array.length; i++){
+    let num = array[i];
+    let diff = target - num;
+    if (!(num in map)) map[num] = i;
+    if (diff in map && map[diff] !== i) {
+      return [map[diff], i];
+    }
+  }
+}
