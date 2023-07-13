f5d422949f6463a8efdf22c4082e3ea173ea0a37
fahd <asadmansa@icloud.com>
2023-07-13 18:15:57 -0400

    added week-1 #18 - reverseLinkedList

---
+++ ✅/week1/18 - reverseLinkedList.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev = null;
+  let node = head;
+  let save;
+
+  while (node) {
+    save = node.next;
+    node.next = prev;
+    prev = node;
+    node = save;
+  }
+  return prev;
+};
--- ✅/week1/7 - validAnagram.ts
+++ ✅/week1/7 - validAnagram.ts
@@ -23,13 +23,13 @@ const validAnagram = (s1: string, s2: string): boolean => {
 
 const isStringAnagram = (s1: string, s2: string): boolean => {
   let map = new Array(26).fill(0);
-  for (let i = 0; i < s1.length; i++){
+  for (let i = 0; i < s1.length; i++) {
     let s1c = s1[i];
     let s2c = s2[i];
-    map[s1c.charCodeAt(0) + 97]++;
-    map[s2c.charCodeAt(0) + 97]--;
+    map[s1c.charCodeAt(0) - 97]++;
+    map[s2c.charCodeAt(0) - 97]--;
   }
-  for (let i = 0; i < map.length; i++){
+  for (let i = 0; i < map.length; i++) {
     if (map[i] !== 0) return false;
   }
   return true;
